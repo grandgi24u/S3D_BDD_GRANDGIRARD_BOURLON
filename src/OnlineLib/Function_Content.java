e920bb1a69d7010aeddd1f6da867cd1258b32d5c
Erwan Bourlon <bourlon.erwan@gmail.com>
2020-11-11 16:25:06 +0100

    reduction des fonctions

--- src/OnlineLib/Function_Content.java
+++ src/OnlineLib/Function_Content.java
@@ -1,4 +1,15 @@
 package OnlineLib;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+
 public class Function_Content {
 	
 	private int recherche;
@@ -21,30 +32,60 @@ public class Function_Content {
 		}
 	}
 
+	public JPanel getPanel() {
+		String[][] list;
+		switch (recherche) {
+			case (1) : list = f1();
+			case (2) : list = f2();
+			case (3) : list = f3();
+			case (4) : list = f4();
+			case (5) : list = f5();
+			default  : list = f6();
+		}
+		DefaultTableModel TableModel = new DefaultTableModel(list, this.getIndex());
+		JTable table = new JTable(TableModel);
+		DefaultTableCellRenderer tableRenderer = new DefaultTableCellRenderer();
+		tableRenderer.setHorizontalAlignment(JLabel.CENTER);
+		table.setDefaultRenderer(Object.class, tableRenderer);
+		
+		JPanel panCentre = new JPanel(new BorderLayout());
+		panCentre.setForeground(Color.blue);
+		panCentre.setBackground(Color.green);
+		panCentre.add(table, BorderLayout.CENTER);
+		JScrollPane scrollpane = new JScrollPane(panCentre);
+
+		return panCentre;
+	}
+
 	public String[] getIndex() {
 		switch (recherche) {
-			case (1) : return i1();
-			case (2) : return i2();
-			case (3) : return i3();
-			case (4) : return i4();
-			case (5) : return i5();
-			default  : return i6();
+			case (1) : return new String[]{"Rank", "Country", "Forested Area(km sq)", "% of land area"};
+			         //return new String[]{"Titre", "Resume", "TypeArticle"};
+			case (2) : return new String[]{"Rank", "Country", "Forested Area(km sq)", "% of land area"};
+       	             //return new String[]{"Email", "NomChercheur", "PrénomChercheur", "UrlChercheur"};
+			case (3) : return new String[]{"Email", "NomChercheur", "PrénomChercheur", "UrlChercheur", "NomLaboratoire", "SigleLaboratoire", "AdresseLaboratoire", "UrlLaboratoire"};
+			case (4) : return new String[]{"Email", "NomChercheur", "PrénomChercheur", "UrlChercheur"};
+			case (5) : return new String[]{};
+			default  : return new String[]{"Email", "NomChercheur", "PrénomChercheur", "UrlChercheur", "NbArticles", "NbNotes", "MoyNotes"};
 		}
 	}
 
 	public String getTitre() {
+		String titre;
 		switch (recherche) {
-			case (1) : return l1();
-			case (2) : return l2();
-			case (3) : return l3();
-			case (4) : return l4();
-			case (5) : return l5();
-			default  : return l6();
+			case (1) : titre = "Détermination de la liste des articles écrits par un auteur donné : "; break;
+			case (2) : titre = "Affichage de la liste des co-auteurs ayant travaillé avec un chercheur : "; break;
+			case (3) : titre = "Affichage de la liste des laboratoires de chaque chercheur : "; break;
+			case (4) : titre = "Affichage la liste des chercheurs ayant annoté au moins un nombre donné d'articles : "; break;
+			case (5) : titre = "Calcul de la moyenne des notes données par un chercheur donné : "; break;
+			default  : titre = "Pour chaque chercheur d'un laboratoire donné, afficher le nombre d'articles publiés, le nombre et la moyenne des notes obtenues : "; break;
 		}
+		return "<html>"+titre+"</html>";
 	}
 
 	private String[][] f1() {
-		String[][] list = new String[][] {
+		return new String[][] {
+			{"Rank", "Country", "Forested Area(km sq)", "% of land area"},
 			{"1", "Russia", "8,149,300", "49.40%"},
 			{"2", "Canada", "4.196,438", "49.24%"},
 			{"3", "Brazil", "4,777,980", "56.10%"},
@@ -66,86 +107,87 @@ public class Function_Content {
 			{"9", "Indonesia", "884,950", "46.46%"},
 			{"10", "India", "778,424", "24.68%"},
 		};
-		return list;
 	}
 
 	private String[][] f2() {
-		String[][] list = new String[][]{};
-		return list;
+		return new String[][] {
+			{"1", "Russia", "8,149,300", "49.40%"},
+			{"2", "Canada", "4.196,438", "49.24%"},
+			{"3", "Brazil", "4,777,980", "56.10%"},
+			{"4", "United States", "2,083,210", "33%"},
+			{"5", "China", "2,083,210", "21.83"},
+			{"6", "Congo", "1,819,326", "50%"},
+			{"7", "Australia", "1,147,832", "19.90%"},
+			{"8", "Argentina", "945,336", "34%"},
+			{"9", "Indonesia", "884,950", "46.46%"},
+			{"10", "India", "778,424", "24.68%"},
+		};
 	}
 
 	private String[][] f3() {
-		String[][] list = new String[][]{};
-		return list;
+		return new String[][]{};
 	}
 
 	private String[][] f4() {
-		String[][] list = new String[][]{};
-		return list;
+		return new String[][]{};
 	}
 
 	private String[][] f5() {
-		String[][] list = new String[][]{};
-		return list;
+		return new String[][]{};
 	}
 
 	private String[][] f6() {
-		String[][] list = new String[][]{};
-		return list;
+		return new String[][]{};
 	}
 	
-	private String[] i1() {
-		String[] index= new String[]{"Rank", "Country", "Forested Area(km sq)", "% of land area"};
-		return index;
-	}
-
-	private String[] i2() {
-		String[] index= new String[]{};
-		return index;
-	}
-
-	private String[] i3() {
-		String[] index= new String[]{};
-		return index;
-	}
-
-	private String[] i4() {
-		String[] index= new String[]{};
-		return index;
-	}
-
-	private String[] i5() {
-		String[] index= new String[]{};
-		return index;
-	}
-
-	private String[] i6() {
-		String[] index= new String[]{};
-		return index;
-	}
-
-	private String l1() {
-		return "Ceci est l'explication de l'utilité de la recherche n°1";
-	}
-
-	private String l2() {
-		return "Ceci est l'explication de l'utilité de la recherche n°2";
-	}
-
-	private String l3() {
-		return "Ceci est l'explication de l'utilité de la recherche n°3";
-	}
-
-	private String l4() {
-		return "Ceci est l'explication de l'utilité de la recherche n°4";
-	}
-
-	private String l5() {
-		return "Ceci est l'explication de l'utilité de la recherche n°5";
-	}
-
-	private String l6() {
-		return "Ceci est l'explication de l'utilité de la recherche n°6";
-	}
+//	private String[] i1() {
+////		return new String[]{"Titre", "Resume", "TypeArticle"};
+//		return new String[]{"Rank", "Country", "Forested Area(km sq)", "% of land area"};
+//	}
+//
+//	private String[] i2() {
+////		return new String[]{"Email", "NomChercheur", "PrénomChercheur", "UrlChercheur"};
+//		return new String[]{"Rank", "Country", "Forested Area(km sq)", "% of land area"};
+//	}
+//
+//	private String[] i3() {
+//		return new String[]{"Email", "NomChercheur", "PrénomChercheur", "UrlChercheur", "NomLaboratoire", "SigleLaboratoire", "AdresseLaboratoire", "UrlLaboratoire"};
+//	}
+//
+//	private String[] i4() {
+//		return new String[]{"Email", "NomChercheur", "PrénomChercheur", "UrlChercheur"};
+//	}
+//
+//	private String[] i5() {
+//		return new String[]{};
+//	}
+//
+//	private String[] i6() {
+//		return new String[]{"Email", "NomChercheur", "PrénomChercheur", "UrlChercheur", "NbArticles", "NbNotes", "MoyNotes"};
+//	}
+//
+//	private String l1() {
+//		return "<html>Détermination de la liste des articles écrits par un auteur donné : </html>";
+//	}
+//
+//	private String l2() {
+//		return "<html>Affichage de la liste des co-auteurs ayant travaillé avec un chercheur : </html>";
+//	}
+//
+//	private String l3() {
+//		return "<html>Affichage de la liste des laboratoires de chaque chercheur : </html>";
+//	}
+//
+//	private String l4() {
+//		return "<html>Affichage la liste des chercheurs ayant annoté au moins un nombre donné d'articles : </html>";
+//	}
+//
+//	private String l5() {
+//		return "<html>Calcul de la moyenne des notes données par un chercheur donné : </html>";
+//	}
+//
+//	private String l6() {
+//		return "<html>Pour chaque chercheur d'un laboratoire donné, afficher le nombre d'articles publiés, le nombre et la moyenne des notes obtenues : </html>";
+//	}
 
 }
---
+++ src/OnlineLib/Principale.java
@@ -0,0 +1,103 @@
+package OnlineLib;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ * Classe principale permettant de créer la fenetre ainsi que son interface graphique 
+ * afin de la remplir grace a d'autres classes externes
+ * @author bourl
+ *
+ */
+public class Principale {
+
+	//Champ permettant de remplir la chaine de caractere necessaire a la recherche
+	private static JTextField BarreDeRecherche;
+	
+	public Principale() {
+		
+		/**
+		 * Selection de la recherche a effectuer (6 sont possibles)
+		 *
+		 *   1 => Détermination de la liste des articles écrits par un auteur donné.
+		 *   2 => Affichage de la liste des co-auteurs ayant travaillé avec un 
+		 *        chercheur donné.
+		 *   3 => Affichage de la liste des laboratoires de chaque chercheur.
+		 *   4 => Affichage la liste des chercheurs ayant annoté au moins un nombre 
+		 *        donné d'articles.
+		 *   5 => Calcul de la moyenne des notes données par un chercheur donné.
+		 *   6 => Pour chaque chercheur d'un laboratoire donné, afficher le nombre 
+		 *        d'articles publiés, le nombre et la moyenne des notes obtenues. 
+		 *        On classera les chercheurs par ordre décroissant du nombre d'articles 
+		 *        publiés
+		 * 
+		 * CHOIX DELA RECHERCHE :
+		 */
+		int recherche = 1;
+		Function_Content fc = new Function_Content(recherche);
+		
+		//JPanel nord
+		JButton menu = new JButton("Menu");
+		JPanel panNord = new JPanel();
+	    panNord.setBackground(new Color(0, 64, 48));
+		JLabel explication  = new JLabel(fc.getTitre());
+		if (recherche==6)
+			explication.setPreferredSize(new Dimension(300, 45));			
+		menu.setForeground(Color.orange);
+	    menu.setBackground(new Color(16, 120, 94));
+	    explication.setForeground(Color.orange);
+		explication.setPreferredSize(new Dimension(300, 30));
+		panNord.add(explication);
+		panNord.add(menu);
+		
+		//JPanel sud
+		JPanel panSud= new JPanel();
+		JButton chercher = new JButton("Chercher");
+		BarreDeRecherche = new JTextField();
+		BarreDeRecherche.setPreferredSize(new Dimension(270, 30));
+	    panSud.setBackground(new Color(0, 64, 48));
+		chercher.setForeground(Color.orange);
+		chercher.setBackground(new Color(16, 120, 94));
+	    BarreDeRecherche.setForeground(Color.orange);
+		BarreDeRecherche.setBackground(new Color(16, 120, 94));
+		panSud.add(BarreDeRecherche);
+		panSud.add(chercher);
+		
+		//JPanel centre
+		DefaultTableModel TableModel = new DefaultTableModel(fc.getListe(), fc.getIndex());
+		JTable table = new JTable(TableModel);
+		DefaultTableCellRenderer tableRenderer = new DefaultTableCellRenderer();
+		tableRenderer.setHorizontalAlignment(JLabel.CENTER);
+		table.setDefaultRenderer(Object.class, tableRenderer);
+		
+		JPanel panCentre = new JPanel(new BorderLayout());
+		panCentre.setForeground(Color.blue);
+		panCentre.setBackground(Color.green);
+		panCentre.add(table, BorderLayout.CENTER);
+		JScrollPane scrollpane = new JScrollPane(panCentre);
+		
+		//Construction de l'interface graphique
+		JFrame f = new JFrame();
+		f.getContentPane().add(panNord, BorderLayout.NORTH);
+//		f.getContentPane().add(scrollpane, BorderLayout.CENTER);
+		f.getContentPane().add(fc.getPanel(), BorderLayout.CENTER);
+		f.getContentPane().add(panSud, BorderLayout.SOUTH);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(new Dimension(400,400));
+		f.setVisible(true);	
+
+	}
+	
+	public static JTextField getBarreDeRecherche() {
+		return BarreDeRecherche;
+	}
+	
+	public static void main (String[] args) {
+		new Principale();
+	}
+
+}
